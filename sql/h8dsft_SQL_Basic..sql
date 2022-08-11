596e6a63c7d7212d136384f034ff5d40a001d24e
hedyfernando <hedy.fernando7@gmail.com>
2022-08-11 21:32:42 +0700

    penambahan file tugas 1

---
+++ sql/h8dsft_SQL_Basic..sql
@@ -0,0 +1,7 @@
+/*Carilah dosen yang memiliki gaji tertinggi per masing-masing mata kuliah. Tampilkan  semua atribut dosen dan semua atribut mata kuliahnya. Urutkan hasilnya berdasarkan nama  mata kuliahnya secara ascending(A-Z).*/
+SELECT teachers.id, teachers.first_name,teachers.last_name,teachers.school,teachers.hire_date,Max(teachers.salary) salary, courses.* 
+from teachers
+INNER JOIN courses
+on teachers.id = courses.teachers_id
+GROUP BY courses.name
+ORDER by courses.name ASC
